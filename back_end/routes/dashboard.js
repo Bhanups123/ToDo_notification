1f60ba074b8761d6dc07e69e91ea1303bd6ab657
Bhanu Pratap Singh <sbhanupratap161@gmail.com>
2020-02-29 07:59:33 +0530

    added signup and login with jwt authorization

---
+++ back_end/routes/dashboard.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const passport = require("passport");
+const app = express();
+const router = express.Router();
+const mongoose = require("mongoose");
+const User = mongoose.model("User");
+
+router.get(
+  "/todo/Dashboard",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const errors = {};
+    console.log(req.user);
+    User.find({ _id: { $ne: req.user.id } }).then(users => {
+      if (users) {
+        res.json(users);
+      } else {
+        errors.notFound = "Not found";
+        res.status(404).json(errors);
+      }
+    });
+  }
+);
+
+router.get(
+  "/todo/Dashboard/:user_id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const errors = {};
+    User.findById(req.params.user_id).then(user => {
+      if (user) {
+        res.json(user);
+      } else {
+        errors.UserNotFound = "user not found";
+        res.status(404).json(errors);
+      }
+    });
+  }
+);
+
+module.exports = router;
---
+++ back_end/routes/user.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const passport = require("passport");
+const User = require("../models/User");
+const Keys = require("../config/keys");
+const router = express.Router();
+
+router.post("/todo/register", (req, res) => {
+  const { name, email, password } = req.body;
+  User.findOne({ email }).then(user => {
+    if (user) {
+      const errors = {};
+      errors.email = "User with this email already exist";
+      res.status(400).json(errors);
+    } else {
+      const newUser = User({
+        name,
+        email,
+        password
+      });
+      bcrypt.genSalt((err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+
+          newUser
+            .save()
+            .then(user => res.json(user))
+            .catch(err => res.json(err));
+        });
+      });
+    }
+  });
+});
+
+router.post("/todo/login", (req, res) => {
+  const { email, password } = req.body;
+  User.findOne({ email }).then(user => {
+    if (!user) {
+      const errors = {};
+      errors.email = "User not found";
+      return res.status(404).json(errors);
+    }
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (!isMatch) {
+        const errors = {};
+        errors.password = "Invalid Password!!";
+        return res.status(401).json(errors);
+      }
+      const payload = {
+        email: user.email,
+        name: user.name
+      };
+      jwt.sign(payload, Keys.secretOrKey, { expiresIn: 3600 }, (err, token) => {
+        if (err) {
+          console.log(err);
+        } else {
+          res.json({ msg: "Congratulation!!!", token: "Bearer " + token });
+        }
+      });
+    });
+  });
+});
+
+module.exports = router;
